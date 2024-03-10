edafabad5f09546157c7fafbae3b362e44ab113b
Parker Knopf <parker.knopf@gmail.com>
2024-03-09 17:59:29 -0800

    Testing bug fixes

--- trajectoryGenerator.m
+++ trajectoryGenerator.m
@@ -4,12 +4,13 @@ function configs = trajectoryGenerator(T_se_i, T_sc_i, T_sc_f, T_ce_g, T_ce_s, k
     T_se_cf_s  = T_sc_f * T_ce_s; % Standoff cube final
 
     T_se_ci_g = T_sc_i * T_ce_g; % Grasp cube initial
+    T_se_cf_g = T_sc_f * T_ce_g; % Drop cube initial
 
-    T0 = {T_se_i, T_se_ci_s, T_se_ci_g, T_se_ci_g, T_se_ci_s, T_se_cf_s, T_sc_f, T_sc_f}; % Inital Configs for every trajectory
+    T0 = {T_se_i, T_se_ci_s, T_se_ci_g, T_se_ci_g, T_se_ci_s, T_se_cf_s, T_se_cf_g, T_se_cf_g, T_se_cf_s}; % Inital Configs for every trajectory
     T1 = {T0{2:end}, T_se_i}; % Final Configs for every trajectory
 
-    grip = [0 0 1 1 1 1 0 0]; % Final Gripper State at every trajectory
-    t = [1 1 1 1 1 1 1 1]; % Time for each trajectory
+    grip = [0 0 1 1 1 1 0 0 0]; % Final Gripper State at every trajectory
+    t = [1 1 1 1 1 1 1 1 1]; % Time for each trajectory
 
     n = 0; % Precontruct Configs Length
     for i = 1:length(t)
