017a7f3daf8f87fa61fceadf24248cf75004ff5f
Parker Knopf <parker.knopf@gmail.com>
2024-03-09 17:27:53 -0800

    patched: missing dt

--- nextState.m
+++ nextState.m
@@ -11,7 +11,7 @@ function x1 = nextState(x0, xDot, dt, xMax)
     end
 
     for i = 9:12
-        x1(i) = x0(i) + xDot(i-3);
+        x1(i) = x0(i) + xDot(i-3)*dt;
     end
 
     u = xDot(6:end)';
