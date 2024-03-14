90cb64ebd7067297f3cf6dc386ae902fb6c23593
19ncramer <74337635+19ncramer@users.noreply.github.com>
2024-03-13 23:01:42 -0700

    Update for working Jb

--- youBotJb.m
+++ youBotJb.m
@@ -2,11 +2,43 @@ function Jb = youBotJb(X, M, B, theta0)
 
     eomg = 0.0001;
     ev = 0.0001;
+    
+    phi = theta0(1);
+    x = theta0(2);
+    y = theta0(3);
+    
+    l = 0.47/2;
+    w = 0.3/2;
+    r = 0.0475;
+    
+    F = [-1/(l + w) 1/(l + w) 1/(l + w) -1/(l + w);
+         1 1 1 1;
+         -1 1 -1 1];
+
+    F6 = [zeros(1,4);
+        zeros(1,4);
+        F.*(r/4);
+        zeros(1,4)];
+
+    T_b0 = [1 0 0 0.1662;
+            0 1 0 0;
+            0 0 1 0.0026;
+            0 0 0 1];
+    
+    T_0s = [cos(phi) -sin(phi) 0 x;
+            sin(phi) cos(phi) 0 y;
+            0 0 1 r;
+            0 0 0 1]^-1;
+     
+    T_0e = T_0s*X;
+
+
+    Jbase = Adjoint(inv(T_0e)*inv(T_b0))*F6;
 
     if size(theta0, 2) > 1
         theta0 = theta0';
     end
 
-    [theta, null] = IKinBody(B, M, X, theta0, eomg, ev);
-    Jb = JacobianBody(B, theta);
+    [theta, null] = IKinBody(B, M, X, theta0(4:8), eomg, ev);
+    Jb = [Jbase JacobianBody(B, theta)]
 end
